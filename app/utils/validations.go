357685e167b81131fa5992128f174efee9deb30a
Ahmad Arif <ahmad.arif019@gmail.com>
2018-02-23 10:10:18 +0700

    add comment for validation util

--- app/utils/validations.go
+++ app/utils/validations.go
@@ -2,11 +2,13 @@ package utils
 
 import "github.com/revel/revel"
 
+// ValidationResponse : simple struct to store the Message & Key of a validation error for response
 type ValidationResponse struct {
 	Key     string `json:"key"`
 	Message string `json:"message"`
 }
 
+// ErrorMapToArray : convert ErrorMap to Array
 func ErrorMapToArray(errorMap map[string]*revel.ValidationError) []ValidationResponse {
 	errors := []ValidationResponse{}
 	for _, v := range errorMap {
